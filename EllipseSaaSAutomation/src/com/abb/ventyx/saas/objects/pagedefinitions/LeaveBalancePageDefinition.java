a913efb7ea124bfd3f98e551999bd5921d708d2d
Sarah PHAM <sarah@enclave.vn>
2017-09-27 16:55:17 +0700

    Sarah update LeaveBalancePageDefinition

--- EllipseSaaSAutomation/src/com/abb/ventyx/saas/objects/pagedefinitions/LeaveBalancePageDefinition.java
+++ EllipseSaaSAutomation/src/com/abb/ventyx/saas/objects/pagedefinitions/LeaveBalancePageDefinition.java
@@ -1,8 +1,11 @@
 package com.abb.ventyx.saas.objects.pagedefinitions;
 
 public class LeaveBalancePageDefinition {
+	
 	public static final String SHOW_ALL_REQUESTS_BUTTON_ID = "searchLeaveRequest";
+	public static final String NEW_LEAVE_REQUESTS_BUTTON_ID = "newLeaveRequest3";
 	public static final String FORECAST_DATE_TEXT_ID = "#forecastDate > input[type='date']";
 	public static final String LEAVE_BALANCE_DAYS_TEXT_ID = "leaveBalanceDays";
 	public static final String LEAVE_BALANCE_HOURS_TEXT_ID = "leaveBalanceHours";
+	
 }
