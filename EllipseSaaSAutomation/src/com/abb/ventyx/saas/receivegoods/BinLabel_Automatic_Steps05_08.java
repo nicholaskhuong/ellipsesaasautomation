68edef7dd66413ffda863808276eb49f5f130d4c
Sarah PHAM <sarah@enclave.vn>
2017-10-11 15:44:14 +0700

    Added new test case 1030 BinLabel_Automatic

---
+++ EllipseSaaSAutomation/src/com/abb/ventyx/saas/receivegoods/BinLabel_Automatic_Steps05_08.java
@@ -0,0 +1,124 @@
+package com.abb.ventyx.saas.receivegoods;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.abb.ventyx.saas.objects.pagedefinitions.ApplicationName;
+import com.abb.ventyx.saas.objects.pagedefinitions.ItemDetailPageDefinition;
+import com.abb.ventyx.saas.objects.pagedefinitions.POItemsPageDefinition;
+import com.abb.ventyx.saas.objects.pagedefinitions.POSearchPageDefinition;
+import com.abb.ventyx.utilities.ALM;
+import com.abb.ventyx.utilities.BaseTestCase;
+import com.abb.ventyx.utilities.Credentials;
+
+@ALM(id = "1030")
+@Credentials(user = "SPR002", password = "", district = "R100", position = "MATMAN")
+public class BinLabel_Automatic_Steps05_08 extends BaseTestCase {
+
+	@Test(description = "Access to Receive Goods Application")
+	public void accessReceiveGoods() {
+		loginToApplication(ApplicationName.RECEIVE_GOODS);
+		Assert.assertEquals(driver.findElement(By.xpath(POSearchPageDefinition.NEW_RECEIVE_GOODS_TEXT_ID)).getText(), "PO Search");
+	}
+
+	@Test(description = "Input value PO Number on PO search Page", dependsOnMethods = "accessReceiveGoods", alwaysRun = true)
+	public void inputPurchaseOrderNumber() {
+		screenAction.waitObjVisible(driver,By.id(POSearchPageDefinition.PO_NUMBER_ID), 3);
+		WebElement purchaseOrderNumber = driver.findElement(By.id(POSearchPageDefinition.PO_NUMBER_ID));
+		purchaseOrderNumber.sendKeys("P05102");
+	}
+
+	@Test(description = "Input value PO Item Number on PO search Page", dependsOnMethods = "inputPurchaseOrderNumber", alwaysRun = true)
+	public void inputPurchaseOrderItemNumber() {
+		WebElement purchaseOrderItemNumber = driver.findElement(By.id(POSearchPageDefinition.PO_ITEM_NUMBER_ID));
+		purchaseOrderItemNumber.sendKeys("1");
+		screenAction.waitObjVisible(driver, By.id(POSearchPageDefinition.PO_ITEM_NUMBER_ID), 3);
+	}
+
+	@Test(description = "Click button SEARCH on PO search Page", dependsOnMethods = "inputPurchaseOrderItemNumber", alwaysRun = true)
+	public void clickSEARCHButton() {
+		screenAction.clickBtn(By.cssSelector(POSearchPageDefinition.SEARCH_BUTTON_ID));
+		screenAction.waitObjVisible(driver, By.cssSelector(POSearchPageDefinition.SEARCH_BUTTON_ID), 4);
+	}
+
+	@Test(description = "Access to PO Items Page", dependsOnMethods = "clickSEARCHButton", alwaysRun = true)
+	public void accessToPOItemsPage() {
+		// Assert.assertEquals(driver.findElement(By.xpath(POItemsPageDefinition.NEW_PO_ITEMS_TEXT_ID)).getText(),"PO Items");
+	}
+
+	@Test(description = "Display Detail Items", dependsOnMethods = "accessToPOItemsPage", alwaysRun = true)
+	public void displayDetailItems() {
+		screenAction.waitObjVisible(driver, By.id(POItemsPageDefinition.PO_ITEM_NUMBER_ID), 3);
+		List<WebElement> documentItem = driver.findElements(By.id(POItemsPageDefinition.PO_ITEM_NUMBER_ID));
+		List<WebElement> partNumber = driver.findElements(By.id(POItemsPageDefinition.PART_NUMBER_ID));
+		List<WebElement> stockCode = driver.findElements(By.id(POItemsPageDefinition.STOCK_CODE_ID));
+		List<WebElement> description = driver.findElements(By.id(POItemsPageDefinition.DESCRIPTION_ID));
+		List<WebElement> outstandingQuantityUOP = driver.findElements(By.id(POItemsPageDefinition.OUT_STANDING_QUANTITY_UOP_ID));
+		List<WebElement> unitOfPurchase = driver.findElements(By.id(POItemsPageDefinition.UNIT_OF_PURCHASE_ID));
+		List<WebElement> outstandingQuantityUOI = driver.findElements(By.id(POItemsPageDefinition.OUT_STANDING_QUANTITY_UOI_ID));
+		List<WebElement> unitOfIssue = driver.findElements(By.id(POItemsPageDefinition.UNIT_OF_ISSUE_ID));
+
+		Assert.assertEquals(documentItem.get(0).getAttribute("value"), "001");
+		Assert.assertEquals(partNumber.get(0).getAttribute("value"), "");
+		Assert.assertEquals(stockCode.get(0).getAttribute("value"), "STC87");
+		Assert.assertEquals(description.get(0).getAttribute("value"), "STOCK CODE;");
+		Assert.assertEquals(outstandingQuantityUOP.get(0).getAttribute("value"), "100.0");
+		Assert.assertEquals(unitOfPurchase.get(0).getAttribute("value"), "EA");
+		Assert.assertEquals(outstandingQuantityUOI.get(0).getAttribute("value"), "100.0");
+		Assert.assertEquals(unitOfIssue.get(0).getAttribute("value"), "EA");
+	}
+
+	@Test(description = "Click button FULL on PO Items Page", dependsOnMethods = "displayDetailItems", alwaysRun = true)
+	public void clickPULLButton() {
+		screenAction.clickBtnByIndex(By.cssSelector(POItemsPageDefinition.FULL_BUTTON_ID), 0);
+	}
+
+	@Test(description = "Display Item Detail Items", dependsOnMethods = "clickPULLButton", alwaysRun = true)
+	public void displayItemDetailItems() {
+		// Assert.assertEquals(driver.findElement(By.xpath(ItemDetailPageDefinition.NEW_ITEM_DETAIL_TEXT_ID)).getText(), "Item Detail");
+		List<WebElement> documentItem = driver.findElements(By.id(ItemDetailPageDefinition.PO_ITEM_NUMBER_ID));
+		List<WebElement> partNumber = driver.findElements(By.id(ItemDetailPageDefinition.PART_NUMBER_ID));
+		List<WebElement> stockCode = driver.findElements(By.id(ItemDetailPageDefinition.STOCK_CODE_ID));
+		List<WebElement> description = driver.findElements(By.id(ItemDetailPageDefinition.DESCRIPTION_ID));
+		List<WebElement> outstandingQuantityUOP = driver.findElements(By.id(ItemDetailPageDefinition.OUT_STANDING_QUANTITY_UOP_ID));
+		List<WebElement> unitOfPurchase = driver.findElements(By.id(ItemDetailPageDefinition.UNIT_OF_PURCHASE_ID));
+		List<WebElement> outstandingQuantityUOI = driver.findElements(By.id(ItemDetailPageDefinition.OUT_STANDING_QUANTITY_UOI_ID));
+		List<WebElement> unitOfIssue = driver.findElements(By.id(ItemDetailPageDefinition.UNIT_OF_ISSUE_ID));
+
+		Assert.assertEquals(documentItem.get(0).getAttribute("value"), "001");
+		Assert.assertEquals(partNumber.get(0).getAttribute("value"), "");
+		Assert.assertEquals(stockCode.get(0).getAttribute("value"), "STC87");
+		Assert.assertEquals(description.get(0).getAttribute("value"), "STOCK CODE;");
+		Assert.assertEquals(outstandingQuantityUOP.get(0).getAttribute("value"), "100.0");
+		Assert.assertEquals(unitOfPurchase.get(0).getAttribute("value"), "EA");
+		Assert.assertEquals(outstandingQuantityUOI.get(0).getAttribute("value"), "100.0");
+		Assert.assertEquals(unitOfIssue.get(0).getAttribute("value"), "EA");
+	}
+	
+	@Test(description = "Input Receipt Reference On Item Detail Page", dependsOnMethods = "displayItemDetailItems", alwaysRun = true)
+	public void inputReceiptReferenceOnItemDetailPage() {
+	screenAction.waitObjVisible(driver,By.cssSelector(ItemDetailPageDefinition.RECEIPT_PEFERENCE2_ID), 3);
+		WebElement receiptReference = driver.findElement(By.cssSelector(ItemDetailPageDefinition.RECEIPT_PEFERENCE2_ID));
+		receiptReference.clear();
+		receiptReference.sendKeys("04may1029");
+	}
+	
+	@Test(description = "Click Receive Button On Item Detail Page", dependsOnMethods = "inputReceiptReferenceOnItemDetailPage", alwaysRun = true)
+	public void clickRECEIVEItem0Button() {
+		screenAction.clickBtn(By.id(ItemDetailPageDefinition.RECEIVE_ID));
+		screenAction.waitObjVisible(driver, By.id(ItemDetailPageDefinition.RECEIVE_ID), 3);
+	}
+
+	@Test(description = "Display Message On PO Item Page", dependsOnMethods = "clickRECEIVEItem0Button", alwaysRun = true)
+	public void displayMessage0OnPOItemPage() {
+			// Assert.assertEquals(driver.findElement(By.xpath(POItemsPageDefinition.NEW_PO_ITEMS_TEXT_ID)).getText(), "PO Items"); // Still error
+		screenAction.waitObjVisible(driver, By.cssSelector(POItemsPageDefinition.MESSAGE_TEXT_ID), 3);	
+		Assert.assertEquals(driver.findElement(By.cssSelector(POItemsPageDefinition.MESSAGE_TEXT_ID)).getText(),"The search did not return any results\n(INFO) CORE.E06004: Action successfully completed.\n(INFO) 3140.I0464: Successfully receipted purchase order. P05102001\n(INFO) 3140.I0491: Bin Label print request submitted for processing");
+		driver.findElement(By.cssSelector(POItemsPageDefinition.MESSAGE_TEXT_ID)).click();
+	}
+}
+
