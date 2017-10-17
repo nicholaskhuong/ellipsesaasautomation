0cc0208dba553c25ec719224a18e11614c6007c9
Sarah PHAM <sarah@enclave.vn>
2017-10-17 13:27:02 +0700

    Add Count Stock in ApplicationName

--- EllipseSaaSAutomation/src/com/abb/ventyx/saas/objects/pagedefinitions/ApplicationName.java
+++ EllipseSaaSAutomation/src/com/abb/ventyx/saas/objects/pagedefinitions/ApplicationName.java
@@ -9,4 +9,5 @@ public class ApplicationName {
 	public static final String PICK_STOCK = "PickStock";
 	public static final String RECEIVE_GOODS = "ReceiveGoods";
 	public static final String WORK_REQUEST = "WorkRequest";
+	public static final String COUNT_STOCK = "CountStock";
 }
